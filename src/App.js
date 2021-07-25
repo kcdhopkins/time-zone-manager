628c6b703cbc4475208b9fa9ce4a55f07639204d
kjay <kcdhopkins@gmail.com>
2021-07-25 10:06:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Grid, Typography } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import img from '../src/images/united-states-time-zone-map.gif'
+const moment = require('moment-timezone')
+
 
 function App() {
+  const [time, setTime] = useState([])
+
+  useEffect(()=>{
+    setInterval(()=>{
+      const currentTime = moment()
+      const PSTtime = moment().tz('America/Los_Angeles').format('h:mm A')
+      const ESTtime = moment().tz('America/New_York').format('h:mm A')
+      const MSTtime = moment().tz('America/Denver').format('h:mm A')
+      const CSTtime = moment().tz('America/New_Orleans').format('h:mm A')
+      setTime([{time: PSTtime, zone:"PST", color: "blue"}, {time:ESTtime, zone: "EST", color: "orange"}, {time:MSTtime, zone:"MST", color: "green"}, {time:CSTtime, zone:"CST", color:"black"}])
+    }, 1000)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid item xs={2}>
+        {time.map( (timeObj, index) => {
+          return (
+            <Box key = {`time-${index}`} component="div" justifyItems="center" alignItems="center" borderRadius={16} border={1} display="flex" flexDirection="column" m={2} boxShadow={3}>
+              <Typography variant="h3" style={{color:timeObj.color}}>
+                {timeObj.zone}
+              </Typography>
+              <Typography variant="h3" style={{color:timeObj.color}}>
+                {timeObj.time}
+              </Typography>
+            </Box>)
+          })
+        }
+      </Grid>
+      <Grid item xs={8}>
+        <img src = {img} alt="timezone_map"/>
+      </Grid>
+    </Grid>
   );
 }
 
