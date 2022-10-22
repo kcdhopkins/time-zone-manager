56a7a066823e18b77bd8ebd77c047c2291f7eae6
kcdhopkins <kcdhopkins@gmail.com>
2022-10-22 10:39:45 -0500

    added header

---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./header.css"
+const Header = () => {
+
+  return (
+    <div id='header' className="d-flex justify-content-center"><span>Nikki's Timezone Manager</span></div>
+  )
+}
+
+export default Header
